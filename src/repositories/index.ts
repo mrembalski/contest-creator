ce6aa801065a691422a79be9707d4fdee7495d20
michal <michal.rembalski@gmail.com>
2021-01-27 13:35:38 +0100

    get to post

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,6 +1,8 @@
 export * from './commission.repository';
 export * from './contest.repository';
+export * from './mark.repository';
 export * from './participation.repository';
+export * from './solution.repository';
 export * from './user.repository';
 
 
